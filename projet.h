ee74a40a20f3312083107b234c5e864afe670b57
ChloeSupmeca <chloe.moro@etu.cyu.fr>
2026-04-19 12:01:27 +0200

    Add dental tools and dentist structure definitions
    Ici on a créé les structures outils, gants et dentiste.

--- projet.h
+++ projet.h
@@ -4,3 +4,19 @@
 
 #define LONGUEUR 20
 #define LARGEUR 10
+
+typedef enum{ // outils dentaires
+      FRAISE_DENTAIRE=1,  
+}Outil;   
+
+typedef enum{  // il s'agit de la présence de gants
+      NON = 0; OUI=1;
+}Gants; 
+
+
+typedef struct{ // création du dentiste
+      int position[x][y];
+      Outil ouinon;
+      Gants gants;
+}Dentiste;
+

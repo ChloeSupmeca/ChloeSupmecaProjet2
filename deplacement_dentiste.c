79ec3599fab31cfeca034d1253bfcf9d1323baee
ChloeSupmeca <chloe.moro@etu.cyu.fr>
2026-05-05 17:41:01 +0200

    Update deplacement_dentiste.c
    ici on ajoute les touches

--- deplacement_dentiste.c
+++ deplacement_dentiste.c
@@ -3,17 +3,22 @@
 void deplacement_Dentiste(Dentist personnage){
     int deplacement;
     
-    if(deplacement=='Q'){
+    scanf("%c" &deplacement);
+    if(deplacement=='Q'|| deplacement=='q'){
         personnage.p.x=-1;
     }
-    if(deplacement=='D'){
+    if(deplacement=='D'|| deplacement=='d'){
         personnage.p.x=1;
     }
-    if(deplacement=='Z'){
+    if(deplacement=='Z'|| deplacement=='z'){
         personnage.p.y=1;
     }
-    if(deplacement=='S'){
+    if(deplacement=='S'|| deplacement=='s'){
         personnage.p.y=-1;
+    }
+    else{
+        printf("erreur !\n");
+        exit(20);
     }
 
 }

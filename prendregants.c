6e1067ca34d835d8192c1b3a5bd13c3376279ae2
ChloeSupmeca <chloe.moro@etu.cyu.fr>
2026-05-05 16:58:33 +0200

    Refactor glove acquisition logic for dentist

--- prendregants.c
+++ prendregants.c
@@ -1,18 +1,22 @@
 #include "projet.h"
 
 void Gants_Dentiste(Dentiste gants){ // ici le dentiste prend des gants
+    Gants objet;
     if(gants.g.gloves==0){
-       printf("Voulez vous prendre des gants ? \n");
-       scanf("%d", &gants.g.gloves);
-       if(gants.g.gloves==0){
-           printf("Vous n'avez pas pris de gants ! \n");
-       }
-       else if(gants.g.gloves==1){
-            printf("Vous avez pris de gants ! \n");
-       }
-       else{
-            printf("Erreur de saisie ! \n");
-       }
+        if(objet=='P'){
+            printf("Voulez vous prendre des gants ? \n");
+           scanf("%d", &gants.g.gloves);
+           if(gants.g.gloves==0){
+               printf("Vous n'avez pas pris de gants ! \n");
+           }
+           else if(gants.g.gloves==1){
+                printf("Vous avez pris de gants ! \n");
+           }
+           else{
+                printf("Erreur de saisie ! \n");
+                   exit(10);
+           }
+        }
     }
     else{
         printf("Vous ne pouvez plus prendre de gants ! \n");

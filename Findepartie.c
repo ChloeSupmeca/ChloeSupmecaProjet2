df26ddb24b3bd7ba4551adcb509f3d2e84e32a96
ChloeSupmeca <chloe.moro@etu.cyu.fr>
2026-05-09 20:01:52 +0200

    Update Findepartie.c
    Fin de la partie

--- Findepartie.c
+++ Findepartie.c
@@ -1,3 +1,11 @@
-if(chair=1 && mecontent=1){
-   printf("Fin de la partie ! \n");
+void fin_de_Partie(Partie *p){
+   int x,y;
+   int occupe;
+   occupe=presence_patient(p, x, y);
+	for (int i=0; i<N_FAUTEUILS; i++){
+      p->patients[i].occupe_fauteuil;
+      if((occupe==1) && (p->patients_mecontents==1)){
+            printf("Fin de la partie ! \n");
+      }
+   }
 }

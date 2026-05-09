4e7373491b2fc4a9fa673d7cf8faf3fe1688b9c5
ChloeSupmeca <chloe.moro@etu.cyu.fr>
2026-05-09 16:23:10 +0200

    Fix patient handling in majPatient function

--- majPatient.c
+++ majPatient.c
@@ -1,12 +1,12 @@
 void majPatient (Partie*p){
 	for (int i=0; i<N_FAUTEUILS; i++){
-		if (p->patient[i].occupe_fauteuil){
-			p->patient[i].patience--;
+		if (p->patients[i].occupe_fauteuil){
+			p->patients[i].patience--;
 		}
-		if (p->patient[i].patience<=0){
+		if (p->patients[i].patience<=0){
 			printf("Patient mécontent parti !");
-			p->patient[i].occupe_fauteuil=0;
-			p->patient_mecontent++;
+			p->patients[i].occupe_fauteuil=0;
+			p->patients_mecontents++;
 		}
 	}
 }				 	

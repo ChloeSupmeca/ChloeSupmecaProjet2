321b2aaa4ec9a983ade5bef9d32573935e114494
Sharani-rjp <sharaniemma@gmail.com>
2026-05-12 16:38:03 +0200

    Update affichagetemporaire.c

--- affichagetemporaire.c
+++ affichagetemporaire.c
@@ -6,3 +6,4 @@ rajouter fonction Chloé gants
 sauvegarde
 ajouter prix des prestations
 commenter le code par bloc au dessus parce que a coté c galere
+bloquer l'acces aux cases instruments poubelles etc

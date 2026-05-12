e1c38d811bb1017dac304db2a6e884ebb4c0379d
ChloeSupmeca <chloe.moro@etu.cyu.fr>
2026-05-12 09:46:30 +0200

    Refactor command handling and improve readability

--- boucleprincipale.c
+++ boucleprincipale.c
@@ -12,17 +12,33 @@ comm = 0;
 if (scanf(" %c", &cmd) != 1) continue;
 
 switch(cmd) {
-case '5': deplacer(p, -1, 0); break; // gauche 
-case '3': deplacer(p, 1, 0); break; // droite 
-case '1': deplacer(p, 0, -1); break; // haut 
-case '2': deplacer(p, 0, 1); break; //bas 
-case 'e': case 'E': action_e(p); break;
-//case 's': case 'S': sauvegarder(p); break;
-case 'x': case 'X':
+case '5':
+    deplacer(p, -1, 0);
+    break; // gauche 
+case '3':
+    deplacer(p, 1, 0);
+    break; // droite 
+case '1':
+    deplacer(p, 0, -1); 
+    break; // haut 
+case '2':
+    deplacer(p, 0, 1); 
+    break; //bas 
+case 'e':
+    case 'E': 
+    action_e(p); 
+        break;
+//case 's': 
+        case 'S': 
+    sauvegarder(p); 
+            break;
+case 'x':
+    case 'X':
 /*printf("Voulez-vous sauvegarder avant de quitter ? (o/n) ");
 char rep;
 scanf(" %c", &rep);
-if (rep == 'o' || rep == 'O') sauvegarder(p);*/
+if (rep == 'o' || rep == 'O') 
+sauvegarder(p);*/
 p->partie_terminee = 1;
 p->score_final = p->argent;
 break;
@@ -32,7 +48,8 @@ break;
 }
 
 p->tours++;
-if (p->tours % 3 == 0) gerer_patience(p); //on diminue la patience tous kes 3 tours
+if (p->tours % 3 == 0){
+    gerer_patience(p); //on diminue la patience tous kes 3 tours
 }
 
 printf("\n----------------------------------\n");
@@ -57,21 +74,26 @@ int prix_prestation(Pathologie* patho) {
 
 int instrument_utile(Patient* pat, Instrument instr) {
     for (int i = 0; i < pat->patho.nb_instruments; i++) {
-        if (pat->patho.instruments[i] == instr) return 1;
+        if (pat->patho.instruments[i] == instr){
+            return 1;
+        }
     }
     return 0;
 }
 
 int instrument_deja_pose(Patient* pat, Instrument instr) {
     for (int i = 0; i < pat->plateau.nb_pose; i++) {
-        if (pat->plateau.pose[i] == instr) return 1;
+        if (pat->plateau.pose[i] == instr){
+            return 1;
     }
     return 0;
+    }
 }
 
 int plateau_complet(Patient* pat) {
     for (int i = 0; i < pat->patho.nb_instruments; i++) {
-        if (!instrument_deja_pose(pat, pat->patho.instruments[i])) return 0;
+        if (!instrument_deja_pose(pat, pat->patho.instruments[i])) {
+            return 0;
     }
     return 1;
 }

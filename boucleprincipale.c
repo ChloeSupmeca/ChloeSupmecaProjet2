084333140bea4484f82eade796fa441d8381f2ef
Sharani-rjp <sharaniemma@gmail.com>
2026-05-12 09:15:36 +0200

    Update boucleprincipale.c

--- boucleprincipale.c
+++ boucleprincipale.c
@@ -1,15 +1,14 @@
 void boucle_jeu(Partie* p) {
-char cmd;
+char comm;
 while (!p->partie_terminee) {
 
 gerer_arrivees(p);
 
 affichage(p);
 
-
 printf("\nAction > ");
 fflush(stdout);
-cmd = 0;
+comm = 0;
 if (scanf(" %c", &cmd) != 1) continue;
 
 switch(cmd) {
@@ -18,12 +17,12 @@ case '3': deplacer(p, 1, 0); break; /* droite */
 case '1': deplacer(p, 0, -1); break; /* haut */
 case '2': deplacer(p, 0, 1); break; /* bas */
 case 'e': case 'E': action_e(p); break;
-case 's': case 'S': sauvegarder(p); break;
+//case 's': case 'S': sauvegarder(p); break;
 case 'x': case 'X':
-printf("Voulez-vous sauvegarder avant de quitter ? (o/n) ");
+/*printf("Voulez-vous sauvegarder avant de quitter ? (o/n) ");
 char rep;
 scanf(" %c", &rep);
-if (rep == 'o' || rep == 'O') sauvegarder(p);
+if (rep == 'o' || rep == 'O') sauvegarder(p);*/
 p->partie_terminee = 1;
 p->score_final = p->argent;
 break;
@@ -33,11 +32,9 @@ break;
 }
 
 p->tours++;
-/* Diminuer patience toutes les 3 actions */
-if (p->tours % 3 == 0) gerer_patience(p);
+if (p->tours % 3 == 0) gerer_patience(p); //on diminue la patience tous kes 3 tours
 }
 
-/* Fin de partie */
 printf("\n----------------------------------\n");
 printf(" FIN DE PARTIE\n");
 printf("------------------------------------\n");
@@ -47,7 +44,5 @@ printf("Satisfaits : %d\n", p->patients_satisfaits);
 printf("Mécontents : %d\n", p->patients_mecontents);
 printf("Furieux : %d\n", p->patients_furieux);
 enregistrer_score(p);
-
-/* Supprimer sauvegarde si partie terminee normalement */
-remove(SAVE_FILE);
+//remove(SAVE_FILE);
 }

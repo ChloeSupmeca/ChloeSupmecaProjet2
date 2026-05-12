6a5067f9738076b438c787531fec6bf5d7b45024
Sharani-rjp <sharaniemma@gmail.com>
2026-05-12 08:28:54 +0200

    Update structures.h

--- structures.h
+++ structures.h
@@ -15,8 +15,36 @@ typedef enum{
     MIROIR,
     SONDE,
     FRAISE,
-    DETARTREUSE
+    DETARTREUSE,
+    NB_INSTRUMENTS
 }Instrument;
+
+typedef enum{
+    CARIES,
+    APHTES,
+    PARODONTITE,
+    GINGIVITE,
+    ABCES,
+    MALOCCLUSION,
+    NB_PATHOLOGIES
+} Pathologie_Type;
+
+typedef enum {
+    SATISFAIT,
+    MECONTENT,
+    FURIEUX
+} Humeur;
+
+typedef enum {
+    SOL,
+    FAUTEUIL,
+    PLATEAU,
+    INSTRUMENTS,
+    RECYCLAGE,
+    BIOLOGIQUE,
+    GANTS_PLACE,
+    MUR
+} Case;
 
 typedef struct{
     Instrument i;
@@ -24,7 +52,7 @@ typedef struct{
 }Etat_Instrument;
 
 typedef struct {
-    int gant;
+    int porte_gant;
     int salete_gants;
 } Gants; 
 
@@ -44,36 +72,38 @@ typedef struct {
     Instrument instruments[2]; 
 } Pathologie; 
 
+typedef struct {
+    Instrument pose[2];
+    int nb_pose;
+    int sale; /* 1 si plateau souille (apres soins) */
+} Table_Patient;
 
 typedef struct {
     int occupe_fauteuil;
     int patience;
-    Etat_Instrument etat_instruments[2];
+    int patience_max;
+    int temps_attente; //temps ecoulé depuis arrivée 
     int traite;
+    Pathologie_Type type_patho;
+    Plateau_Patient plateau;
+    int plateau_pose;   
+    int gratuit;//patient furieux part sans payer
     Position p;
     Pathologie patho;
 } Patient;
 
-typedef enum {
-    SOL,
-    FAUTEUIL,
-    PLATEAU,
-    INSTRUMENTS,
-    RECYCLAGE,
-    BIOLOGIQUE,
-    GANTS,
-    MUR
-} Case;
+
 
 typedef struct {
     Case grille[LONGUEUR][HAUTEUR];
     Patient patients[N_FAUTEUILS];
     Dentiste dentiste;
     int argent;
-    float temps;
+    int temps;
     int patients_satisfaits;
     int patients_mecontents;
     int patients_furieux;
-
-
+    int partie_terminee;
+    int score_final;
+    time_t debut_partie;
 } Partie;

b8209319df81b41169b63c21f38da408417ec1da
Sharani-rjp <sharaniemma@gmail.com>
2026-05-13 16:49:24 +0200

    Update affichagetemporaire.c

--- affichagetemporaire.c
+++ affichagetemporaire.c
@@ -2,8 +2,5 @@
 choses a rajouter :
 verifier les scanf
 gestion des fichiers
-rajouter fonction Chloé gants
-sauvegarde
-ajouter prix des prestations
 commenter le code par bloc au dessus parce que a coté c galere
 bloquer l'acces aux cases instruments poubelles etc

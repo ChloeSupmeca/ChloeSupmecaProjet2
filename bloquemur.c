52ca2755781a8c1e77fe80b6cb3074de72d8aeef
ChloeSupmeca <chloe.moro@etu.cyu.fr>
2026-05-14 17:09:59 +0200

    Update case_bloquante to include new cases
    on bloque aussi les poubelles

--- bloquemur.c
+++ bloquemur.c
@@ -1,5 +1,6 @@
 int case_bloquante(Case c) {
-   return c == MUR || c == FAUTEUIL || c == PATIENT || c == PORTE_FERMEE;
+
+    return c == MUR || c == FAUTEUIL || c == INSTRUMENTS || c == RECYCLAGE || c == BIOLOGIQUE || c == GANTS_CASE;
 }
 
 void deplacer(Partie* p, int dx, int dy) {
@@ -10,7 +11,7 @@ void deplacer(Partie* p, int dx, int dy) {
     /* Hors limites */
     if (nx < 0 || nx >= LONGUEUR ||ny < 0 || ny >= HAUTEUR){
         return;
-     }
+    }
     /* Collision */
     if (case_bloquante(p->grille[nx][ny])){
         return;
@@ -18,4 +19,3 @@ void deplacer(Partie* p, int dx, int dy) {
     p->dentiste.p.x = nx;
     p->dentiste.p.y = ny;
 }
-

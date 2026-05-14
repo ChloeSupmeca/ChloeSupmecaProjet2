45aca36b99b83c8a903c3c04f9deab2c66aa4d0a
Sharani-rjp <sharaniemma@gmail.com>
2026-05-14 16:10:33 +0200

    Update affichagetemporaire.c

--- affichagetemporaire.c
+++ affichagetemporaire.c
@@ -4,4 +4,4 @@ verifier les scanf
 gestion des fichiers
 commenter le code par bloc au dessus parce que a coté c galere
 bloquer l'acces aux cases instruments poubelles etc
-creer un fichier score
+

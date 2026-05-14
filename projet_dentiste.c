4c7a262131f21a026dfce4bbe2366b7615eb25b5
Sharani-rjp <sharaniemma@gmail.com>
2026-05-14 18:05:23 +0200

    Update projet_dentiste.c

--- projet_dentiste.c
+++ projet_dentiste.c
@@ -882,7 +882,11 @@ int ny = p->dentiste.p.y + dy;
 if (nx < 0 || nx >= LONGUEUR || ny < 0 || ny >= HAUTEUR) return;
 if (p->grille[nx][ny] == MUR) return;
 if (p->grille[nx][ny] == FAUTEUIL) return; /* ne peut pas marcher sur un fauteuil */
-
+if (p->grille[nx][ny] == PLATEAU) return;
+if (p->grille[nx][ny] == INSTRUMENTS) return;
+if (p->grille[nx][ny] == RECYCLAGE) return;
+if (p->grille[nx][ny] == BIOLOGIQUE) return;
+if (p->grille[nx][ny] == GANTS_CASE) return;
 p->dentiste.p.x = nx;
 p->dentiste.p.y = ny;
 }

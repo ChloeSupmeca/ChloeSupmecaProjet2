18aeec4fd60558fd94e8dfdf154e99fcfc3ee961
ChloeSupmeca <chloe.moro@etu.cyu.fr>
2026-05-19 18:43:56 +0200

    Add static data for instruments and pathologies

---
+++ donnees_statiques.c
@@ -0,0 +1,12 @@
+/* ===================== DONNEES STATIQUES ===================== */
+
+
+
+
+const char* noms_instruments[NB_INSTRUMENTS] = {
+"AUCUN", "PINCE", "ECARTEURS", "SERINGUE", "MIROIR", "SONDE", "FRAISE", "DETARTREUSE"
+};
+
+const char* noms_pathologies[NB_PATHOLOGIES] = {
+"Caries", "Aphtes", "Parodontite", "Gingivite", "Abces", "Malocclusion"
+};

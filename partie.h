409237514f24bd32c78f9ad36567c81dbdc20800
Sharani-rjp <sharaniemma@gmail.com>
2026-05-24 10:58:35 +0200

    Update partie.h

--- partie.h
+++ partie.h
@@ -3,26 +3,26 @@
 
 #include "types.h"
 
-/* ---- Initialisation ---- */
+/* Initialisation */
 void init_grille(Partie* p);
 void nouvelle_partie(Partie* p);
 
-/* ---- Chargement fichiers ---- */
+/*  Chargement fichiers */
 Instrument instrument_depuis_nom(char* nom);
 void charger_couts(Partie* p);
 void charger_pathologies(Partie* p);
 
-/* ---- Patients ---- */
+/*  Patients  */
 void faire_arriver_patient(Partie* p);
 void gerer_arrivees(Partie* p);
 void gerer_patience(Partie* p);
 
-/* ---- Sauvegarde ---- */
+/*  Sauvegarde  */
 void sauvegarder(Partie* p);
 int  charger_sauvegarde(Partie* p);
 int  fichier_sauvegarde_existe(void);
 
-/* ---- Scores ---- */
+/*  Scores  */
 void enregistrer_score(Partie* p);
 
 #endif /* PARTIE_H */
